d05240f735e9ed0a7054b7d47c8017571a0fbc59
xufx <1454130786@qq.com>
2017-07-26 21:52:19 +0800

    âDept、Job、EmployeeController

---
+++ src/main/java/org/fkit/hrm/controller/DeptController.java
@@ -0,0 +1,7 @@
+package org.fkit.hrm.controller;
+/**
+ * Created by xufuxiu on 2017/7/25.
+ */
+public class DeptController
+{
+}
---
+++ src/main/java/org/fkit/hrm/controller/JobController.java
@@ -0,0 +1,7 @@
+package org.fkit.hrm.controller;
+/**
+ * Created by xufuxiu on 2017/7/26.
+ */
+public class JobController
+{
+}
